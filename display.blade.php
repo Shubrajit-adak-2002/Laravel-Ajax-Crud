f94859833f5c756132a06ab003493e72c5fac6be
Shubrajit-adak-2002 <139214846+Shubrajit-adak-2002@users.noreply.github.com>
2025-03-18 06:44:17 +0530

    Add files via upload
    better version of display

--- display.blade.php
+++ display.blade.php
@@ -1,129 +1,150 @@
-<div>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <table border="1">
-        <thead>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<table border="1">
+    <thead>
+        <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Image</th>
             <th>Action</th>
-        </thead>
-        <tbody id="data"></tbody>
-    </table>
-    <div id="edit-f"></div>
+        </tr>
+    </thead>
+    <tbody id="data">
+    </tbody>
+</table>
+<style>
+    #modal {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        background-color: rgb(0, 0, 0);
+        background-color: rgba(0, 0, 0, 0.4);
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        padding-top: 150px;
+        padding-left: 50px;
+    }
+
+    #modal-content {
+        background-color: #fefefe;
+        margin: auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 40%;
+        border-radius: 8px;
+    }
+
+    #close {
+        float: right;
+        background-color: red;
+        padding: 5px;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+
+    h3 {
+        text-align: center;
+    }
+</style>
+
+<div id="modal">
+    <div id="modal-content">
+        <span id="close">X</span>
+        <h3>Edit Form</h3>
+        <form id="e-form" enctype="multipart/form-data">
+        </form>
+    </div>
 </div>
 <script>
-    $(document).ready(function() {
-        $.ajax({
-            url: "{{ route('display') }}",
-            method: "GET",
-            success: function(data) {
-                console.log(data);
-                if (data.students.length > 0) {
-                    for (let i = 0; i < data.students.length; i++) {
-                        let image = data.students[i]['image']
-                        let imgPath = image.includes('upload/') ? "{{ asset('') }}" + image :
-                            "{{ asset('upload') }}/" + image
-
-                        $('#data').append(`
-                            <tr>
-                                <td>` + (data.students[i]['name']) + `</td>
-                                <td>` + (data.students[i]['email']) + `</td>
-                                <td><img src="${imgPath}" height="100px" width="100px" alt="Student Image"></td>
-                                <td><button class="edit-btn" data-id="` + data.students[i]['id'] + `">Edit</button>
-                                     | <button class="delete-btn" data-id="${data.students[i]['id']}">Delete</button></td>
-                            </tr>
-                        `)
-
-                    }
-                } else {
-                    $('#table').append(`
-                        <tr>
-                            <td colspan="4">No Data Found</td>
-                        </tr>
-                    `)
-                }
-            },
-            error: function(err) {
-                console.log(err);
-            }
-        })
-
-        $(document).on('click', '.edit-btn', function(e) {
-            e.preventDefault()
-            let id = $(this).data('id');
-            console.log(id);
-
-            $.ajax({
-                url: "{{ route('edit',':id') }}".replace(':id',id),
-                method: "GET",
-                success: function(data) {
-                    if (data.student) {
-                        $('#edit-f').html(`
-                        <form enctype="multipart/form-data" id="e-form">
-                            @csrf
-                            Name : <input type="text" name="name" value="`+(data.student['name'])+`" id="name"><br>
-                            Email : <input type="text" name="email" value="`+(data.student['email'])+`" id="email"><br>
-                            <input type="file" name="image" id="image"><br>
-                            <button id="update" data-id="${data.student['id']}">Update</button>
-                        </form>
-                        `)
-                    }else {
-                        $('#edit-f').html('<p>Student not found.</p>');
-                    }
-                },
-                error:function(xhr){
-                    console.error("Responded Text",xhr.responseText);
-                }
-            })
-        })
-
+    $(function(){
         function load(){
             $.ajax({
-                url: "{{ route('display') }}",
-                method: "GET",
-                success: function (response) {
-                    let html = '';
-                    response.students.forEach(student => {
+                url:"{{route('data')}}",
+                method:"GET",
+                success:function(res){
+                   let html = ''
+
+                    res.data.forEach(student => {
                         html += `
                             <tr>
                                 <td>${student.name}</td>
                                 <td>${student.email}</td>
-                                <td><img src="${student.image}" alt="Image" width="50"></td>
-                                <td>
-                                    <button class="edit-btn" data-id="${student.id}">Edit</button>
-                                    | <button class="delete-btn" data-id="${student.id}">Delete</button></td>
-                                </td>
+                                <td><img src='${student.image}' height='100px' width='100px'/></td>
+                                <td><button class='edit' data-id=${student.id}>Edit</button> | <button class='delete' data-id=${student.id}>Delete</button></td>
                             </tr>
-                        `;
+                        `
                     });
-                    $('#data').html(html);
+                    $('#data').html(html)
                 },
                 error:function(xhr){
-                    console.error("Error"+xhr.responseJSON.error);
+                    console.log(xhr.responsJSON.error);
+
                 }
             })
         }
-
         load()
+
+        $(document).on('click','#close',function(e){
+            e.preventDefault()
+
+            $('#modal').hide()
+        })
+
+        $(document).on('click','.edit',function(e){
+            e.preventDefault()
+            $('#modal').show()
+
+            let id = $(this).data('id')
+
+            $.ajax({
+                url:"{{route('edit',':id')}}".replace(':id',id),
+                method:"GET",
+                data:{id:id},
+                success:function(data){
+                    if (data.edit) {
+                        $('#e-form').html(`
+                            @csrf
+                            <input type='text' id='name' value='${data.edit['name']}'><br>
+                            <input type='text' id='email' value='${data.edit['email']}'><br>
+                            <input type='file' id='image'><br>
+                            <input type='submit' id='update' data-id=${data.edit['id']}>
+                        `)
+                    }
+                },
+                error:function(err){
+                    console.error(err.responseJSON.error);
+
+                }
+            })
+        })
 
         $(document).on('click','#update',function(e){
             e.preventDefault()
 
-            let form = $('#e-form')[0]
-            let id = $(this).data('id')
+            let form = new FormData()
 
-            let formData = new FormData(form)
+            let id = $(this).data('id')
+            console.log(id);
+
+
+            form.append('_token','{{csrf_token()}}')
+            form.append('_method','PATCH')
+            form.append('name',$('#name').val())
+            form.append('email',$('#email').val())
+            form.append('image',$('#image')[0].files[0])
 
             $.ajax({
                 url:"{{route('update',':id')}}".replace(':id',id),
                 method:"POST",
-                data:formData,
+                data:form,
                 processData:false,
                 contentType:false,
                 success:function(data){
-                    alert('Data updated')
                     load()
+                    $('#modal').hide()
                 },
                 error:function(xhr){
                     console.error("Error"+xhr.responseJSON.error);
@@ -131,14 +152,16 @@
             })
         })
 
-        $(document).on('click','.delete-btn',function(e){
+        $(document).on('click','.delete',function(e){
             e.preventDefault()
+
             let id = $(this).data('id')
+
             let self = this
 
             $.ajax({
                 url:"{{route('delete',':id')}}".replace(':id',id),
-                method:"POST",
+                method:"DELETE",
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') // Add CSRF token here
                 },
